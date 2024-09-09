7f706f538f1647cfc4623c13db7128fdecbc75e4
hanzel98 <hanzel98@hotmail.com>
2024-09-09 15:41:40 -0600

    fix base64url import

--- src/libraries/utils/Base64URL.sol
+++ src/libraries/utils/Base64URL.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT AND Apache-2.0
 pragma solidity 0.8.23;
 
-import "openzeppelin-contracts/contracts/utils/Base64.sol";
+import { Base64 } from "@openzeppelin/contracts/utils/Base64.sol";
 
 library Base64URL {
     function encode(bytes memory data) internal pure returns (string memory) {
