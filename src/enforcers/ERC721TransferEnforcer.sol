0ec045cdb9e6ff394f5e7a84b7a3d412963bc217
Dylan DesRosier <dylandesrosier@gmail.com>
2024-09-30 12:19:59 -0400

    chore: more review tweaks

--- src/enforcers/ERC721TransferEnforcer.sol
+++ src/enforcers/ERC721TransferEnforcer.sol
@@ -7,13 +7,11 @@ import { IERC721 } from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import { ExecutionLib } from "@erc7579/lib/ExecutionLib.sol";
 
 contract ERC721TransferEnforcer is CaveatEnforcer {
-    error UnauthorizedTransfer();
-
     function beforeHook(
         bytes calldata _terms,
         bytes calldata,
-        ModeCode,
-        bytes calldata executionCallData,
+        ModeCode _mode,
+        bytes calldata _executionCallData,
         bytes32,
         address,
         address
@@ -21,37 +19,38 @@ contract ERC721TransferEnforcer is CaveatEnforcer {
         public
         virtual
         override
+        onlySingleExecutionMode(_mode)
     {
-        (address permittedContract, uint256 permittedTokenId) = getTermsInfo(_terms);
-        (address target,, bytes calldata callData_) = ExecutionLib.decodeSingle(executionCallData);
-        bytes4 selector = bytes4(callData_[0:4]);
+        (address permittedContract_, uint256 permittedTokenId_) = getTermsInfo(_terms);
+        (address target_,, bytes calldata callData_) = ExecutionLib.decodeSingle(_executionCallData);
+        bytes4 selector_ = bytes4(callData_[0:4]);
 
         // Decode the remaining callData into NFT transfer parameters
         if (callData_.length < 100) {
             revert("ERC721TransferEnforcer:invalid-calldata-length");
         }
 
-        address from;
-        address to;
-        uint256 transferTokenId;
-        (from, to, transferTokenId) = abi.decode(callData_[4:], (address, address, uint256));
+        address from_;
+        address to_;
+        uint256 transferTokenId_;
+        (from_, to_, transferTokenId_) = abi.decode(callData_[4:], (address, address, uint256));
 
-        if (from == address(0) || to == address(0)) {
+        if (from_ == address(0) || to_ == address(0)) {
             revert("ERC721TransferEnforcer:invalid-address");
         }
 
-        if (target != permittedContract) {
+        if (target_ != permittedContract_) {
             revert("ERC721TransferEnforcer:unauthorized-contract-target");
-        } else if (selector != IERC721.transferFrom.selector) {
+        } else if (selector_ != IERC721.transferFrom.selector) {
             revert("ERC721TransferEnforcer:unauthorized-selector");
-        } else if (transferTokenId != permittedTokenId) {
+        } else if (transferTokenId_ != permittedTokenId_) {
             revert("ERC721TransferEnforcer:unauthorized-token-id");
         }
     }
 
-    function getTermsInfo(bytes calldata _terms) public pure returns (address permittedContract, uint256 permittedTokenId) {
+    function getTermsInfo(bytes calldata _terms) public pure returns (address permittedContract_, uint256 permittedTokenId_) {
         if (_terms.length != 52) revert("ERC721TransferEnforcer:invalid-terms-length");
-        permittedContract = address(bytes20(_terms[:20]));
-        permittedTokenId = uint256(bytes32(_terms[20:]));
+        permittedContract_ = address(bytes20(_terms[:20]));
+        permittedTokenId_ = uint256(bytes32(_terms[20:]));
     }
 }
