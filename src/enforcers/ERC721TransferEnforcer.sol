22fa23ced0333dc22e8b303d04a20ecbf4034d34
Dylan DesRosier <dylandesrosier@gmail.com>
2024-09-30 11:40:52 -0400

    chore: nit picky pr comments

--- src/enforcers/ERC721TransferEnforcer.sol
+++ src/enforcers/ERC721TransferEnforcer.sol
@@ -17,32 +17,29 @@ contract ERC721TransferEnforcer is CaveatEnforcer {
         bytes32,
         address,
         address
-    ) public virtual override {
+    )
+        public
+        virtual
+        override
+    {
         (address permittedContract, uint256 permittedTokenId) = getTermsInfo(_terms);
-        (address target, uint256 _value, bytes calldata callData_) = ExecutionLib.decodeSingle(executionCallData);
+        (address target,, bytes calldata callData_) = ExecutionLib.decodeSingle(executionCallData);
         bytes4 selector = bytes4(callData_[0:4]);
+
         // Decode the remaining callData into NFT transfer parameters
         if (callData_.length < 100) {
             revert("ERC721TransferEnforcer:invalid-calldata-length");
         }
-        
+
         address from;
         address to;
         uint256 transferTokenId;
-        
-        if (callData_.length < 100) {
-            revert("ERC721TransferEnforcer:invalid-calldata-length");
-        }
-        
         (from, to, transferTokenId) = abi.decode(callData_[4:], (address, address, uint256));
-        
-        if (from == address(0) || to == address(0)) {
-            revert("ERC721TransferEnforcer:invalid-address");
-        }
 
         if (from == address(0) || to == address(0)) {
             revert("ERC721TransferEnforcer:invalid-address");
         }
+
         if (target != permittedContract) {
             revert("ERC721TransferEnforcer:unauthorized-contract-target");
         } else if (selector != IERC721.transferFrom.selector) {
@@ -51,16 +48,6 @@ contract ERC721TransferEnforcer is CaveatEnforcer {
             revert("ERC721TransferEnforcer:unauthorized-token-id");
         }
     }
-
-    function afterHook(
-        bytes calldata,
-        bytes calldata,
-        ModeCode,
-        bytes calldata,
-        bytes32,
-        address,
-        address
-    ) public virtual override {}
 
     function getTermsInfo(bytes calldata _terms) public pure returns (address permittedContract, uint256 permittedTokenId) {
         if (_terms.length != 52) revert("ERC721TransferEnforcer:invalid-terms-length");
