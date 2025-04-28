8cda2fec17c9bbc34454da63c29ab235a61facad
Hanzel Anchia Mena <33629234+hanzel98@users.noreply.github.com>
2025-04-28 08:12:31 -0600

    Audit April 2025 - 4.12 - Fixed typo in error message tokenMismatch (#98)

--- test/helpers/DelegationMetaSwapAdapter.t.sol
+++ test/helpers/DelegationMetaSwapAdapter.t.sol
@@ -1045,7 +1045,7 @@ contract DelegationMetaSwapAdapterMockTest is DelegationMetaSwapAdapterBaseTest 
         DelegationMetaSwapAdapter.SignatureData memory sigData_ = _buildSigData(validApiData_);
 
         vm.prank(address(subVault.deleGator));
-        vm.expectRevert(DelegationMetaSwapAdapter.TokenFromMismath.selector);
+        vm.expectRevert(DelegationMetaSwapAdapter.TokenFromMismatch.selector);
         delegationMetaSwapAdapter.swapByDelegation(sigData_, delegations_, true);
     }
 
@@ -1085,7 +1085,7 @@ contract DelegationMetaSwapAdapterMockTest is DelegationMetaSwapAdapterBaseTest 
         DelegationMetaSwapAdapter.SignatureData memory sigData_ = _buildSigData(apiData_);
 
         vm.prank(address(subVault.deleGator));
-        vm.expectRevert(DelegationMetaSwapAdapter.AmountFromMismath.selector);
+        vm.expectRevert(DelegationMetaSwapAdapter.AmountFromMismatch.selector);
         delegationMetaSwapAdapter.swapByDelegation(sigData_, delegations_, true);
     }
 
