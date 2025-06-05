9cc7f19cfbd18aaf17509820e1083ebccf4e2c04
hanzel98 <hanzel.mena@consensys.net>
2025-06-05 05:34:58 -0600

    test: adding possible scenarios

--- test/enforcers/BalancePayment.t.sol
+++ test/enforcers/BalancePayment.t.sol
@@ -6,9 +6,10 @@ import { BasicERC20 } from "../utils/BasicERC20.t.sol";
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import { ExecutionLib } from "@erc7579/lib/ExecutionLib.sol";
 import { ModeLib } from "@erc7579/lib/ModeLib.sol";
+import { ExecutionHelper } from "@erc7579/core/ExecutionHelper.sol";
 
 import "../../src/utils/Types.sol";
-import { Execution, Caveat, Delegation, ModeCode } from "../../src/utils/Types.sol";
+import { Execution, Caveat, Delegation, ModeCode, CallType, ExecType } from "../../src/utils/Types.sol";
 import { CaveatEnforcerBaseTest } from "./CaveatEnforcerBaseTest.t.sol";
 import { ERC20TransferAmountEnforcer } from "../../src/enforcers/ERC20TransferAmountEnforcer.sol";
 import { ERC20BalanceChangeEnforcer } from "../../src/enforcers/ERC20BalanceChangeEnforcer.sol";
@@ -17,6 +18,8 @@ import { IDelegationManager } from "../../src/interfaces/IDelegationManager.sol"
 import { ExactCalldataEnforcer } from "../../src/enforcers/ExactCalldataEnforcer.sol";
 import { AllowedTargetsEnforcer } from "../../src/enforcers/AllowedTargetsEnforcer.sol";
 import { ValueLteEnforcer } from "../../src/enforcers/ValueLteEnforcer.sol";
+import { CALLTYPE_SINGLE, EXECTYPE_DEFAULT } from "../../src/utils/Constants.sol";
+import { console2 } from "forge-std/console2.sol";
 
 contract BalancePaymentTest is CaveatEnforcerBaseTest {
     ////////////////////////////// State //////////////////////////////
@@ -48,8 +51,8 @@ contract BalancePaymentTest is CaveatEnforcerBaseTest {
         vm.label(someUser, "Some User");
 
         // Deploy test tokens
-        tokenA = new BasicERC20(delegator, "TokenA", "TKA", 1 ether);
-        tokenB = new BasicERC20(delegate, "TokenB", "TKB", 2 ether);
+        tokenA = new BasicERC20(delegator, "TokenA", "TKA", 100 ether);
+        tokenB = new BasicERC20(delegate, "TokenB", "TKB", 100 ether);
         vm.label(address(tokenA), "Token A");
         vm.label(address(tokenB), "Token B");
         swapMock.setTokens(address(tokenA), address(tokenB));
@@ -69,8 +72,11 @@ contract BalancePaymentTest is CaveatEnforcerBaseTest {
 
     ////////////////////// Test Cases //////////////////////
 
-    /// @notice Tests that the balance change enforcer reverts when the required payment in TokenB is not received
-    /// after sending TokenA. This simulates a failed swap where TokenB is not sent back to the delegator.
+    /// @notice Tests enforcement of minimum balance increase requirement in a token swap scenario
+    /// @dev Verifies that ERC20BalanceChangeEnforcer correctly reverts when:
+    ///      1. TokenA is sent from delegator to SwapMock
+    ///      2. The required minimum increase in TokenB balance is not met
+    /// This test ensures the enforcer properly protects against failed or incomplete swaps
     function test_failWhenPaymentNotReceived() public {
         // Create delegation from Alice to SwapMock allowing transfer of 1 ETH worth of TokenA
         bytes memory transferTerms_ = abi.encodePacked(address(tokenA), uint256(1 ether));
@@ -141,7 +147,7 @@ contract BalancePaymentTest is CaveatEnforcerBaseTest {
         innerDelegations_[0] = innerDelegation_;
 
         // Create second delegation with exact calldata for swap function
-        bytes memory balanceTerms_ = abi.encodePacked(false, address(tokenB), address(delegator), uint256(2 ether));
+        bytes memory balanceTerms_ = abi.encodePacked(false, address(tokenB), address(delegator), uint256(1 ether));
         Caveat[] memory outerCaveats_ = new Caveat[](4);
         outerCaveats_[0] = Caveat({
             args: hex"",
@@ -178,18 +184,79 @@ contract BalancePaymentTest is CaveatEnforcerBaseTest {
             address(swapMock), 0, abi.encodeWithSelector(SwapMock.swap.selector, innerDelegations_, 1 ether)
         );
 
-        assertEq(tokenA.balanceOf(address(delegator)), 1 ether, "TokenA balance of delegator should be 1 ether");
+        assertEq(tokenA.balanceOf(address(delegator)), 100 ether, "TokenA balance of delegator should be 100 ether");
         assertEq(tokenB.balanceOf(address(delegator)), 0 ether, "TokenB balance of delegator should be 0 ether");
         assertEq(tokenA.balanceOf(address(swapMock)), 0 ether, "TokenA balance of swapMock should be 0 ether");
-        assertEq(tokenB.balanceOf(address(swapMock)), 2 ether, "TokenB balance of swapMock should be 2 ether");
+        assertEq(tokenB.balanceOf(address(swapMock)), 100 ether, "TokenB balance of swapMock should be 100 ether");
 
         vm.prank(someUser);
         delegationManager.redeemDelegations(permissionContexts_, encodedModes_, executionCallDatas_);
 
-        assertEq(tokenA.balanceOf(address(delegator)), 0, "TokenA balance of delegator should be 0");
-        assertEq(tokenB.balanceOf(address(delegator)), 2 ether, "TokenB balance of delegator should be 2 ether");
+        assertEq(tokenA.balanceOf(address(delegator)), 99 ether, "TokenA balance of delegator should be 99 ether");
+        assertEq(tokenB.balanceOf(address(delegator)), 1 ether, "TokenB balance of delegator should be 1 ether");
         assertEq(tokenA.balanceOf(address(swapMock)), 1 ether, "TokenA balance of swapMock should be 1 ether");
-        assertEq(tokenB.balanceOf(address(swapMock)), 0, "TokenB balance of swapMock should be 0");
+        assertEq(tokenB.balanceOf(address(swapMock)), 99 ether, "TokenB balance of swapMock should be 99 ether");
+    }
+
+    /**
+     * @notice Tests state sharing vulnerability in ERC20BalanceChangeEnforcer when batching delegations
+     * @dev This test demonstrates an issue where multiple delegations checking the same token
+     *      balance on the same recipient can lead to unintended behavior. Specifically:
+     *      1. Two delegations each require a balance increase of 1 tokenB
+     *      2. Each delegation transfers 1 tokenA
+     *      3. Only 1 tokenB is received in return (instead of expected 2)
+     *      4. Test passes incorrectly because both delegations reference the same balance state
+     */
+    function test_batchingRedemptionsEnforcerShareState() public {
+        // Create delegation from Alice to SwapMock allowing transfer of 1 ETH worth of TokenA
+        bytes memory transferTerms_ = abi.encodePacked(address(tokenA), uint256(1 ether));
+        bytes memory balanceTerms_ = abi.encodePacked(false, address(tokenB), address(delegator), uint256(2 ether));
+
+        Caveat[] memory caveats_ = new Caveat[](2);
+        // Allows to transfer 1 ETH worth of TokenA
+        caveats_[0] = Caveat({ args: hex"", enforcer: address(transferAmountEnforcer), terms: transferTerms_ });
+        // Requires the balance of the recipient to increase by 2 ETH worth of TokenB
+        caveats_[1] = Caveat({ args: hex"", enforcer: address(balanceChangeEnforcer), terms: balanceTerms_ });
+
+        Delegation memory delegation1 = Delegation({
+            delegate: delegate,
+            delegator: delegator,
+            authority: ROOT_AUTHORITY,
+            caveats: new Caveat[](0),
+            salt: 0,
+            signature: hex""
+        });
+
+        Delegation memory delegation2 = Delegation({
+            delegate: delegate,
+            delegator: delegator,
+            authority: ROOT_AUTHORITY,
+            caveats: new Caveat[](0),
+            salt: 1,
+            signature: hex""
+        });
+
+        delegation1 = signDelegation(users.alice, delegation1);
+        delegation2 = signDelegation(users.alice, delegation2);
+
+        // Creating two redemption flows
+        Delegation[][] memory delegations_ = new Delegation[][](2);
+        delegations_[0] = new Delegation[](1);
+        delegations_[0][0] = delegation1;
+        delegations_[1] = new Delegation[](1);
+        delegations_[1][0] = delegation2;
+
+        assertEq(tokenA.balanceOf(address(delegator)), 100 ether, "TokenA balance of delegator should be 100 ether");
+        assertEq(tokenB.balanceOf(address(delegator)), 0 ether, "TokenB balance of delegator should be 0 ether");
+        assertEq(tokenA.balanceOf(address(swapMock)), 0 ether, "TokenA balance of swapMock should be 0 ether");
+        assertEq(tokenB.balanceOf(address(swapMock)), 100 ether, "TokenB balance of swapMock should be 100 ether");
+
+        swapMock.swapDoubleSpend(delegations_, 1 ether);
+
+        assertEq(tokenA.balanceOf(address(delegator)), 98 ether, "TokenA balance of delegator should be 98 ether");
+        assertEq(tokenB.balanceOf(address(delegator)), 1 ether, "TokenB balance of delegator should be 1 ether");
+        assertEq(tokenA.balanceOf(address(swapMock)), 2 ether, "TokenA balance of swapMock should be 2 ether");
+        assertEq(tokenB.balanceOf(address(swapMock)), 99 ether, "TokenB balance of swapMock should be 99 ether");
     }
 
     // Override helper from BaseTest
@@ -198,22 +265,34 @@ contract BalancePaymentTest is CaveatEnforcerBaseTest {
     }
 }
 
-contract SwapMock {
+contract SwapMock is ExecutionHelper {
     IDelegationManager public delegationManager;
     IERC20 public tokenIn;
     IERC20 public tokenOut;
+
+    error NotSelf();
+    error UnsupportedCallType(CallType callType);
+    error UnsupportedExecType(ExecType execType);
+
+    /**
+     * @notice Require the function call to come from the this contract itself.
+     */
+    modifier onlySelf() {
+        if (msg.sender != address(this)) revert NotSelf();
+        _;
+    }
 
     constructor(IDelegationManager _delegationManager) {
         delegationManager = _delegationManager;
     }
 
-    function setTokens(address _tokenIn, address _tokenOut) public {
+    function setTokens(address _tokenIn, address _tokenOut) external {
         tokenIn = IERC20(_tokenIn);
         tokenOut = IERC20(_tokenOut);
     }
 
-    // This contract swaps X amount of tokensIn for double amount of tokensOut
-    function swap(Delegation[] memory _delegations, uint256 _amountIn) public {
+    // This contract swaps X amount of tokensIn for the amount of tokensOut
+    function swap(Delegation[] memory _delegations, uint256 _amountIn) external {
         bytes[] memory permissionContexts_ = new bytes[](1);
         permissionContexts_[0] = abi.encode(_delegations);
 
@@ -234,7 +313,92 @@ contract SwapMock {
         uint256 balanceTokenIn_ = tokenIn.balanceOf(address(this));
         require(balanceTokenIn_ >= _amountIn, "SwapMock:insufficient-balance-in");
 
-        // Transfer the double amount of tokensOut to the caller after receiving the tokensIn
-        tokenOut.transfer(msg.sender, _amountIn * 2);
+        // This is a big assumption
+        address recipient_ = _delegations[0].delegator;
+        // Transfer the amount of tokensOut to the recipient after receiving the tokensIn
+        tokenOut.transfer(recipient_, _amountIn);
+    }
+
+    // This contract swaps X amount of tokensIn for amount of tokensOut
+    function swapDoubleSpend(Delegation[][] memory _delegations, uint256 _amountIn) external {
+        uint256 length_ = _delegations.length;
+        bytes[] memory permissionContexts_ = new bytes[](length_ + 1);
+        for (uint256 i = 0; i < length_; i++) {
+            permissionContexts_[i] = abi.encode(_delegations[i]);
+        }
+        permissionContexts_[length_] = abi.encode(new Delegation[](0));
+
+        ModeCode[] memory encodedModes_ = new ModeCode[](length_ + 1);
+        for (uint256 i = 0; i < length_ + 1; i++) {
+            encodedModes_[i] = ModeLib.encodeSimpleSingle();
+        }
+
+        bytes memory executionCalldata_ = ExecutionLib.encodeSingle(
+            address(tokenIn), 0, abi.encodeWithSelector(IERC20.transfer.selector, address(this), _amountIn)
+        );
+        bytes[] memory executionCallDatas_ = new bytes[](length_ + 1);
+        for (uint256 i = 0; i < length_; i++) {
+            executionCallDatas_[i] = executionCalldata_;
+        }
+
+        // This is a big assumption
+        // The first delegation.delegate is the one that will receive the tokensOut
+        executionCallDatas_[length_] = ExecutionLib.encodeSingle(
+            address(this),
+            0,
+            abi.encodeWithSelector(this.validateAndTransfer.selector, _delegations[0][0].delegator, length_, _amountIn)
+        );
+
+        delegationManager.redeemDelegations(permissionContexts_, encodedModes_, executionCallDatas_);
+    }
+
+    /**
+     * @notice Validates that enough tokens were received and transfers output tokens to caller
+     * @dev Can only be called by this contract itself via onlySelf modifier
+     * @param _delegationsLength The number of delegations that were processed
+     * @param _amountIn The input amount per delegation
+     */
+    function validateAndTransfer(address _recipient, uint256 _delegationsLength, uint256 _amountIn) external onlySelf {
+        // Some condition representing the need for the ERC20 tokensIn at this point
+        uint256 balanceTokenIn_ = tokenIn.balanceOf(address(this));
+
+        // Required the amount in multiple times
+        require(balanceTokenIn_ >= _amountIn * _delegationsLength, "SwapMock:insufficient-balance-in");
+
+        tokenOut.transfer(_recipient, _amountIn);
+    }
+
+    /**
+     * @notice Executes one calls on behalf of this contract,
+     *         authorized by the DelegationManager.
+     * @dev Only callable by the DelegationManager. Supports single-call execution,
+     *         and handles the revert logic via ExecType.
+     * @dev Related: @erc7579/MSAAdvanced.sol
+     * @param _mode The encoded execution mode of the transaction (CallType, ExecType, etc.).
+     * @param _executionCalldata The encoded call data (single) to be executed.
+     * @return returnData_ An array of returned data from each executed call.
+     */
+    function executeFromExecutor(
+        ModeCode _mode,
+        bytes calldata _executionCalldata
+    )
+        external
+        payable
+        returns (bytes[] memory returnData_)
+    {
+        require(msg.sender == address(delegationManager), "SwapMock:not-delegation-manager");
+
+        (CallType callType_, ExecType execType_,,) = ModeLib.decode(_mode);
+
+        // Only support single call type with default execution
+        if (CallType.unwrap(CALLTYPE_SINGLE) != CallType.unwrap(callType_)) revert UnsupportedCallType(callType_);
+        if (ExecType.unwrap(EXECTYPE_DEFAULT) != ExecType.unwrap(execType_)) revert UnsupportedExecType(execType_);
+
+        // Process single execution directly without additional checks
+        (address target_, uint256 value_, bytes calldata callData_) = ExecutionLib.decodeSingle(_executionCalldata);
+
+        returnData_ = new bytes[](1);
+        returnData_[0] = _execute(target_, value_, callData_);
+        return returnData_;
     }
 }
