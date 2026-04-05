57b5b88c10f5a5a64163f084c2c97532a11f63b7
MoMannn <tadej.vengust@consensys.net>
2026-04-05 20:05:01 +0200

    move deploy script to .env variables

--- script/DeployVedaAdapter.s.sol
+++ script/DeployVedaAdapter.s.sol
@@ -9,29 +9,30 @@ import { VedaAdapter } from "../src/helpers/VedaAdapter.sol";
 /**
  * @title DeployVedaAdapter
  * @notice Deploys the VedaAdapter contract.
- * @dev Update the hardcoded addresses below before deploying.
- * @dev Fill the SALT variable in the .env file
+ * @dev Fill the required variables in the .env file
  * @dev run the script with:
  * forge script script/DeployVedaAdapter.s.sol --rpc-url <your_rpc_url> --private-key $PRIVATE_KEY --broadcast
  */
 contract DeployVedaAdapter is Script {
-    // Hardcoded constructor parameters - update these before deploying
-    address constant OWNER = address(0x0000000000000000000000000000000000000000);
-    address constant DELEGATION_MANAGER = address(0x0000000000000000000000000000000000000000);
-    address constant BORING_VAULT = address(0x0000000000000000000000000000000000000000);
-    address constant VEDA_TELLER = address(0x0000000000000000000000000000000000000000);
-
     bytes32 salt;
     address deployer;
+    address vedaAdapterOwner;
+    address delegationManager;
+    address boringVault;
+    address vedaTeller;
 
     function setUp() public {
         salt = bytes32(abi.encodePacked(vm.envString("SALT")));
+        vedaAdapterOwner = vm.envAddress("VEDA_ADAPTER_OWNER_ADDRESS");
+        delegationManager = vm.envAddress("DELEGATION_MANAGER_ADDRESS");
+        boringVault = vm.envAddress("BORING_VAULT_ADDRESS");
+        vedaTeller = vm.envAddress("VEDA_TELLER_ADDRESS");
         deployer = msg.sender;
         console2.log("~~~");
-        console2.log("Owner: %s", OWNER);
-        console2.log("DelegationManager: %s", DELEGATION_MANAGER);
-        console2.log("BoringVault: %s", BORING_VAULT);
-        console2.log("VedaTeller: %s", VEDA_TELLER);
+        console2.log("Owner: %s", vedaAdapterOwner);
+        console2.log("DelegationManager: %s", delegationManager);
+        console2.log("BoringVault: %s", boringVault);
+        console2.log("VedaTeller: %s", vedaTeller);
         console2.log("Deployer: %s", deployer);
         console2.log("Salt:");
         console2.logBytes32(salt);
@@ -41,7 +42,7 @@ contract DeployVedaAdapter is Script {
         console2.log("~~~");
         vm.startBroadcast();
 
-        address vedaAdapter = address(new VedaAdapter{ salt: salt }(OWNER, DELEGATION_MANAGER, BORING_VAULT, VEDA_TELLER));
+        address vedaAdapter = address(new VedaAdapter{ salt: salt }(vedaAdapterOwner, delegationManager, boringVault, vedaTeller));
         console2.log("VedaAdapter: %s", vedaAdapter);
 
         vm.stopBroadcast();
